a8427822ac4a3c436bfc336e3b8a402345f8fd49
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2025-06-15 18:27:33 +0900

    include

--- jupyter/nb/source/pl07_hand_compile/include/asm/struct_array_field_assign.s
+++ jupyter/nb/source/pl07_hand_compile/include/asm/struct_array_field_assign.s
@@ -8,19 +8,13 @@
 struct_array_field:
 .LFB0:
 	.cfi_startproc
-	adrp	x2, .LC0
+	mov	x2, 30
 	mov	x1, 50
-	ldr	q0, [x2, #:lo12:.LC0]
-	str	q0, [x0, 240]!
-	str	x1, [x0, 16]
+	str	x2, [x0, 240]
+	str	x1, [x0, 256]
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	struct_array_field, .-struct_array_field
-	.section	.rodata.cst16,"aM",@progbits,16
-	.align	4
-.LC0:
-	.xword	30
-	.xword	40
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
