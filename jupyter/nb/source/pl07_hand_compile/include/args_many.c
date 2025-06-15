4c590035e8feed48161dbd7b303e1b85eb758275
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2025-06-15 10:54:34 +0900

    include

--- jupyter/nb/source/pl07_hand_compile/include/args_many.c
+++ jupyter/nb/source/pl07_hand_compile/include/args_many.c
@@ -1,24 +1,10 @@
-long add_many(long a, long b, long c, long d, long e, long f,
-              long g, long h, long i, long j, long k, long l,
-              long m, long n, long o, long p, long q, long r) {
-  long x = 0;
-  x += a;
-  x += b;
-  x += c;
-  x += d;
-  x += e;
-  x += f;
-  x += g;
-  x += h;
-  x += i;
-  x += j;
-  x += k;
-  x += l;
-  x += m;
-  x += n;
-  x += o;
-  x += p;
-  x += q;
-  x += r;
-  return x;
+long add_many(long a00, long a01, long a02, long a03, long a04, 
+              long a05, long a06, long a07, long a08, long a09,
+              long a10, long a11, long a12, long a13, long a14,
+              long a15, long a16, long a17, long a18, long a19) {
+  return (a00 + a01 + a02 + a03 + a04
+          + a05 + a06 + a07 + a08 + a09
+          + a10 + a11 + a12 + a13 + a14
+          + a15 + a16 + a17 + a18 + a19);
 }
+
---
+++ jupyter/nb/source/pl07_hand_compile/include/arith.c
@@ -0,0 +1,31 @@
+long add(long x, long y, long z) {
+  return x + 2 * y + 3 * z;
+}
+
+long sub(long x, long y, long z) {
+  return x - y - z;
+}
+
+long mul(long x, long y, long z) {
+  return x * y * z;
+}
+
+long div(long x, long y, long z) {
+  return x / y / z;
+}
+
+double add(double x, double y, double z) {
+  return x + 2 * y + 3 * z;
+}
+
+double sub(double x, double y, double z) {
+  return x - y - z;
+}
+
+double mul(double x, double y, double z) {
+  return x * y * z;
+}
+
+double div(double x, double y, double z) {
+  return x / y / z;
+}
