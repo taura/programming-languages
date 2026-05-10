f134a00a4a57eb7b8e37c8318170be52b2206720
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2026-05-10 21:24:43 +0900

    pl05

--- jupyter2/answers/tail_recursion/tail_recurrence/jl/tail_recurrence.jl
+++ jupyter2/answers/tail_recursion/tail_recurrence/jl/tail_recurrence.jl
@@ -26,10 +26,10 @@ affine_recurrence_fast_tail(a, b, c, n) = affine_recurrence_fast_tail_rec(a, b, 
 ### end my answer
 
 function main()
-    @assert abs(affine_recurrence_fast_tail(0.9999, 1.0, 2.0, 100000)  - 9999.5463184) < 1.0e-6
+    @assert abs(affine_recurrence_fast_tail(0.9999,   1.0, 2.0, 100000)  - 9999.5463184) < 1.0e-6
     @assert abs(affine_recurrence_simple_tail(0.9999, 1.0, 2.0, 100000)  - 9999.5463184) < 1.0e-6
-    @assert abs(affine_recurrence_fast_tail(0.9999, 1.0, 2.0, 1000000) - 10000.0) < 1.0e-6
-    @assert abs(affine_recurrence_simple_tail(0.9999, 1.0, 2.0, 1000000) - 10000.0) < 1.0e-6
+    @assert abs(affine_recurrence_fast_tail(0.9999,   1.0, 2.0, 1000000) - 10000.0) < 1.0e-6
+    @assert abs(affine_recurrence_simple_tail(0.9999, 1.0, 2.0, 500000)  - 10000.0) < 1.0e-6
     println("OK")
 end
 
